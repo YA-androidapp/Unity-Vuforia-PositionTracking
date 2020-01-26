4e8b2f5c76deda367da2435ec4550293ca61f185
YA <YA-androidapp@users.noreply.github.com>
2020-01-26 22:41:35 +0900

    Add many moving cubes

---
+++ Unity-Vuforia-PositionTracking/Assets/GenManyObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenManyObjects : MonoBehaviour
+{
+    public GameObject targetGameObject;
+    public float MinRange, MaxRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= 500; i++) {
+            Instantiate(
+                targetGameObject,
+                new Vector3(Random.Range(MinRange, MaxRange),
+                    Random.Range(MinRange, MaxRange),
+                    Random.Range(MinRange, MaxRange)
+                ),
+                Quaternion.Euler(
+                    Random.Range(0, 180),
+                    Random.Range(0, 180),
+                    Random.Range(0, 180)
+                )
+            );
+        }
+    }
+}
